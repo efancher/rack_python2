2ae28e393e372de673180a10762ef19ccdbb2d15
student <student@localhost.localdomain>
2014-12-01 10:43:01 -0600

    Finished lab 1.

--- lab01/lab1.py
+++ lab01/lab1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import argparse
-
+from random import randint
 parser = argparse.ArgumentParser(description='Patent database search engine')
 parser.add_argument('--author',
 		   nargs='*', 
@@ -14,3 +14,23 @@ parser.add_argument('--filing_date',
                    help='Filing Date')
 args = parser.parse_args()
 print(args)
+
+def test_db_load():
+   functions = [find_by_author, find_by_patent_number, find_by_filing_date]
+   while True:
+   	yield functions[randint(0,2)]
+
+def find_by_author():
+   pass
+
+def find_by_patent_number():
+   pass
+
+def find_by_filing_date():
+   pass
+
+next_gen = test_db_load()
+print(next(next_gen))
+print(next(next_gen))
+
+print(next(next_gen))
